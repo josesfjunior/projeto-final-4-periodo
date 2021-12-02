9b4011fa76cc8542e717d852d8382a2ab871c6d8
José Júnior <72272209+josesfjunior@users.noreply.github.com>
2021-12-02 16:06:56 -0300

    Remoção do front e atualização do projeto

--- src/main/java/br/edu/cesmac/projetofinal/repository/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.edu.cesmac.projetofinal.repository;
-
-import br.edu.cesmac.projetofinal.model.ProdutoModel;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProdutoRepository extends CrudRepository <ProdutoModel, Integer> {
-}
